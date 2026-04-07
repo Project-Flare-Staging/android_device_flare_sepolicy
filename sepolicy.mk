8f700f6bda6eec805f263fb66da93717abf3fd3b
CuriousNom <aryanbiswas7585@gmail.com>
2026-04-07 23:32:33 +0530

    sepolicy: Allow perf service to find surfaceflinger
    E SELinux : avc:  denied  { find } for pid=1210 uid=1000 name=vendor.perfservice scontext=u:r:surfaceflinger:s0 tcontext=u:object_r:vendor_perf_service:s0 tclass=service_manager permissive=0
    
    Change-Id: I2a1a1006710fc8f58e20ff0e0064185684c1e9dc

--- sepolicy.mk
+++ sepolicy.mk
@@ -2,8 +2,8 @@
 BOARD_VENDOR_SEPOLICY_DIRS += \
     device/flare/sepolicy/legacy/vendor
 
-#SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-#    device/flare/sepolicy/legacy/private
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    device/flare/sepolicy/legacy/private
 
 #SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
 #    device/flare/sepolicy/legacy/public
