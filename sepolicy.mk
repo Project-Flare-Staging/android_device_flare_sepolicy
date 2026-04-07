58f5a8788d8dd0c40a07b599078bd9035a7905d2
Juhyung Park <qkrwngud825@gmail.com>
2026-04-08 00:00:02 +0530

    sepolicy: Allow setting ro.vendor.lmkd.* properties
    Change-Id: Ia45e4d2554e96333ed751352c05db883038fda2e

--- sepolicy.mk
+++ sepolicy.mk
@@ -1,9 +1,9 @@
 # Additional Flare Sepolicy
-#BOARD_VENDOR_SEPOLICY_DIRS += \
-#    device/flare/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    device/flare/sepolicy/vendor
 
-#SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-#    device/flare/sepolicy/private
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    device/flare/sepolicy/private
 
 #SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
 #    device/flare/sepolicy/public
