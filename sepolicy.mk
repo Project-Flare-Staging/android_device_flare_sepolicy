b8efe3c5c53a50cceb3a08be4d153a2a2e223863
ProjectFlare-OSS <projectflare2000@gmail.com>
2026-04-13 11:40:39 +0530

    sepolicy: Initialize Flare Sepolicy
    * We will store additional sepolicy for legacy devices here.
    
    Change-Id: I5ed809e4d7949a1aeba3c86ee1d5221a908e8016

---
+++ sepolicy.mk
@@ -0,0 +1,9 @@
+# Additional Flare Sepolicy
+#BOARD_VENDOR_SEPOLICY_DIRS += \
+#    device/flare/sepolicy/legacy/vendor
+
+#SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+#    device/flare/sepolicy/legacy/private
+
+#SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+#    device/flare/sepolicy/legacy/public
